c21ff856c4bd57b0c05e6eee04f007916e2c6c1b
Sarath13-dev <ramsssjrs@gmail.com>
2021-08-29 19:34:59 +0530

    master

---
+++ src/org/sel/Employee.java
@@ -0,0 +1,13 @@
+package org.sel;
+
+public class Employee {
+
+	
+	private void employeeId() {
+		System.out.println("employee id is 2345");
+	}
+	public static void main(String[] args) {
+		Employee a = new Employee();
+		a.employeeId();
+	}
+}
